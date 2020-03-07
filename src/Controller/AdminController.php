116e6d00a9a552750f0103e2aacfc6c0d4ca75ba
Gremy <le_gremy@hotmail.com>
2020-03-07 15:52:21 +0100

    Passwords encryption with easyAdmin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AdminController extends EasyAdminController
+{
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    protected function persistUserEntity($user)
+    {
+        $encodedPassword = $this->encodePassword($user, $user->getPassword());
+        $user->setPassword($encodedPassword);
+
+        parent::persistEntity($user);
+    }
+
+    protected function updateUserEntity($user, $encoder)
+    {
+        $encodedPassword = $this->encodePassword($user, $user->getPassword());
+        $user->setPassword($encodedPassword);
+
+        parent::updateEntity($user);
+    }
+
+    private function encodePassword($user, $password)
+    {
+        return $this->encoder->encodePassword($user, $password);
+    }
+}
